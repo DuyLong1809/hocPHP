5ad8606fa12d9d65903f31d401f783c9ccc45a4b
DuyLong1809 <115103500+DuyLong1809@users.noreply.github.com>
2023-03-26 18:47:08 +0700

    testcode

--- baitap.php
+++ baitap.php
@@ -1,1 +1,18 @@
 <?php
+class long{
+
+    protected function GetName(){
+        return $this->name;
+    }
+
+    public function FullName(){
+        return $this->GetName();
+    }
+}
+
+class duy{
+
+    public function AAA(){
+        return $this -> GetName();
+    }
+}
